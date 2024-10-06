0fb2b54e3c866464403cfc42109376301159e7d5
TheQiBandit <164599241+TheQiBandit@users.noreply.github.com>
2024-10-06 10:40:15 +0300

    Add comments
    Explain my logic

--- Stock-Picker.rb
+++ Stock-Picker.rb
@@ -1,11 +1,14 @@
-
-
 def stock_picker(price_array)
   profit = 0
   best_days = [nil, nil]
 
+  #Iterate through the array
   price_array.each_with_index do |buy_price, buy_day|
+    
+    #Iterate through the buy day
     price_array.each_with_index do |sell_price, sell_day|
+
+      #Set conditionals to ignore sell days before last days and update max profit
       if (sell_day > buy_day && sell_price - buy_price > profit)
         profit = sell_price - buy_price
         best_days = [buy_day, sell_day]       
